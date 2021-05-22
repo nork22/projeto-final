d9deb038151652ae02663f3a6510d522b8c9a758
nork22 <78579839+nork22@users.noreply.github.com>
2021-05-21 23:37:12 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,37 +1,45 @@
-'use strict';
 
-const fs = require('fs');
-const path = require('path');
-const Sequelize = require('sequelize');
-const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
-const db = {};
+const express = require ('express');
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+const apiRouter = require('./routes/api') //Arquivo de rotas
 
-fs
-  .readdirSync(__dirname)
-  .filter(file => {
-    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
-  })
-  .forEach(file => {
-    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
-    db[model.name] = model;
-  });
+const app = express();
 
-Object.keys(db).forEach(modelName => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
-});
+const colors = require('colors');
 
-db.sequelize = sequelize;
-db.Sequelize = Sequelize;
+const cors = require('cors');
 
-module.exports = db;
+
+
+require('./db'); //Requerimento de nossa BD
+
+
+
+
+
+
+
+/* Middleware para trocar informações, A ordem de carregamento do middleware é importante,
+
+as funções de middleware que carregam primeiro também são executadas primeiro.*/
+
+
+
+app.use(cors())
+
+app.use(express.json())
+
+app.use(express.urlencoded({ extended: true}));
+
+app.use('/api', apiRouter) 
+
+/* Nossa porta do APi */
+
+
+
+app.listen(5000, () =>{
+
+    console.log('SERVER ON PORT 5000'.bgGreen.black)
+    
+    })
+    
